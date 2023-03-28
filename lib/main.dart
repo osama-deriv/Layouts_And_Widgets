c97447b8e759aeace97f2e1460af855721a55047
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-03-28 17:03:21 +0800

    deriv image container with background

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Center(
-      child: SizedBox(
+      child: Container(
         height: 100,
         width: 100,
+        color: Colors.white,
         child: Image.network(
             'https://global.discourse-cdn.com/business7/uploads/deriv/original/1X/d3d6e390e7775f74826935d6c34b8fd870611a40.png'),
       ),
