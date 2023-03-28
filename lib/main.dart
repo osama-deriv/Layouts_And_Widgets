57f0b37b132f39469eabc8b25f6a607f0ae4d4e7
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-03-28 17:03:55 +0800

    add padding

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
         height: 100,
         width: 100,
         color: Colors.white,
+        padding: const EdgeInsets.all(10),
+
         child: Image.network(
             'https://global.discourse-cdn.com/business7/uploads/deriv/original/1X/d3d6e390e7775f74826935d6c34b8fd870611a40.png'),
       ),
