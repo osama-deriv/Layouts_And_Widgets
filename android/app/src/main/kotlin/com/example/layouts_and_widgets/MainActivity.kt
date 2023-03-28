c0c89a069781c93642494af300ee25cd86123a67
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-03-28 15:37:42 +0800

    hello deriv

---
+++ android/app/src/main/kotlin/com/example/layouts_and_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.layouts_and_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, Deriv!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+       ),
+    ),
+  );
+}
