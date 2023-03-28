907ef14167aaa64649e7b3870100fa1ea53644d6
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-03-28 17:23:56 +0800

    deriv in row

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        color: Colors.white,
-        padding: const EdgeInsets.all(10),
-
-        child: Image.network(
-            'https://global.discourse-cdn.com/business7/uploads/deriv/original/1X/d3d6e390e7775f74826935d6c34b8fd870611a40.png'),
+      child: Row(
+        textDirection:TextDirection.ltr ,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            padding: const EdgeInsets.all(10),
+            child: Image.network(
+                'https://global.discourse-cdn.com/business7/uploads/deriv/original/1X/d3d6e390e7775f74826935d6c34b8fd870611a40.png'),
+          ),
+          const Text(
+            'Deriv',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 40),
+          )
+        ],
       ),
     ),
   );
