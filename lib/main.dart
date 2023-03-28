89e5a39637a90890deabdec42510ce65c56a5a73
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-03-28 17:32:40 +0800

    use stack

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        textDirection:TextDirection.ltr ,
+      child: Stack(
+        textDirection: TextDirection.ltr,
         children: [
           Container(
-            height: 100,
-            width: 100,
+            height: 200,
+            width: 200,
             padding: const EdgeInsets.all(10),
             child: Image.network(
                 'https://global.discourse-cdn.com/business7/uploads/deriv/original/1X/d3d6e390e7775f74826935d6c34b8fd870611a40.png'),
