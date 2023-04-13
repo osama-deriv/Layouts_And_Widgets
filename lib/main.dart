4b26ec8631d5563b1e28164edf48666c3401d99a
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2023-04-13 15:19:50 +0800

    Exercise1 Solution

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,60 @@ void main() {
           title: Text('My Layout'),
         ),
         body: Container(
-          padding: const EdgeInsets.all(16.0),
-          // start write your code here starting by add child to this container
-          // child: ...
+          padding: EdgeInsets.all(16.0),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Welcome to My Layout!',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      flutterLogo(),
+                      SizedBox(width: 16.0),
+                      flutterLogo(),
+                      SizedBox(width: 16.0),
+                      flutterLogo(),
+                      SizedBox(width: 16.0),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'This is a simple example of how to use Flutter widgets to build layouts. With the help of the Row and Column widgets, we can easily arrange widgets horizontally and vertically. The Stack widget is useful for overlaying widgets on top of each other. And with the Container widget, we can easily add padding and other styling to our layout.',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      favIcon(),
+                      starIcon(),
+                      likeIcon(),
+                    ],
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Text(
+                  'Created by Me',
+                  style: TextStyle(
+                    fontSize: 12.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
